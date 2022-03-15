d153dbec5ecfc2651d777f3deba675027242afd7
ctronp <carlostp7@uc.cl>
2022-03-14 22:51:12 -0300

    fish and some minor changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /usr/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
 if status is-interactive
     starship init fish | source
 end
--- .config/fish/functions/kc.fish
+++
@@ -1,3 +0,0 @@
-function kc --wraps=kubectl --description 'alias kc kubectl'
-  kubectl $argv; 
-end
