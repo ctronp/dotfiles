a2d878da086035ed571e36740cd6fb291021c4dc
ctronp <carlostp7@uc.cl>
2022-05-13 12:14:50 -0400

    supt

--- .config/fish/functions/supt.fish
+++ .config/fish/functions/supt.fish
@@ -1,3 +1,3 @@
-function supt --wraps='sudo dnf up && flatpak update' --description 'alias supt sudo dnf up && flatpak update'
-  sudo dnf up && flatpak update $argv; 
+function supt --wraps='sudo dnf up --refresh && flatpak update && rustup update && cupdate' --description 'alias supt sudo dnf up --refresh && flatpak update && rustup update && cupdate'
+  sudo dnf up --refresh && flatpak update && rustup update && cupdate $argv; 
 end
