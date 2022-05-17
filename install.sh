c8b3bb36626b89e4135f3b7228b30f07146b088b
ctronp <carlostp7@uc.cl>
2022-05-17 11:39:57 -0400

    clang as default compiler and ld

--- install.sh
+++ install.sh
@@ -2,3 +2,12 @@
 
 # minimal install packages
 sudo dnf install git git-lfs clang clang lld
+
+# replace gcc with clang and ld with lld
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 50
+
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 50
+
+sudo ln -sf /usr/bin/ld.lld /usr/bin/ld
