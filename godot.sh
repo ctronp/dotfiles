502c6a60872a2928bd7e424fe378deab92ec2314
ctronp <carlostp7@uc.cl>
2022-06-16 17:14:45 -0400

    godot compiler and installer

--- godot.sh
+++ godot.sh
@@ -2,7 +2,6 @@
 
 cd ~/godot
 
-set -xe
 
 printf "
 module_mono_enable = \"yes\"
@@ -14,3 +13,13 @@ git pull
 scons platform=linuxbsd target=release_debug module_mono_enabled=yes mono_glue=no bits=64
 bin/godot.linuxbsd.opt.tools.64.mono --generate-mono-glue modules/mono/glue
 scons platform=linuxbsd target=release_debug module_mono_enabled=yes bits=64
+cp -f misc/dist/linux/org.godotengine.Godot.desktop ~/.local/share/applications/
+
+sed -i "s|^Exec=godot|Exec=$HOME/godot/bin/godot.linuxbsd.opt.tools.64.mono|1" ~/.local/share/applications/org.godotengine.Godot.desktop
+sed -i "s|^Icon=godot|Icon=$HOME/godot/icon.svg|1" ~/.local/share/applications/org.godotengine.Godot.desktop
+
+
+if command -v fish
+then
+  cp -f ~/godot/misc/dist/shell/godot.fish ~/.config/fish/completions 
+fi
