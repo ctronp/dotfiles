98ce99bac0aeca23db51b0d4b9e421857fabf220
ctronp <carlostp7@uc.cl>
2022-06-16 16:32:39 -0400

    double compilation for godot to enable mono without bugs

--- godot.sh
+++ godot.sh
@@ -2,9 +2,15 @@
 
 cd ~/godot
 
+set -xe
+
 printf "
 module_mono_enable = \"yes\"
 deprecated = \"no\"
 " > custom.py
 
-scons platform=linuxbsd target=release_debug bits=64
+
+git pull
+scons platform=linuxbsd target=release_debug module_mono_enabled=yes mono_glue=no bits=64
+bin/godot.linuxbsd.opt.tools.64.mono --generate-mono-glue modules/mono/glue
+scons platform=linuxbsd target=release_debug module_mono_enabled=yes bits=64
