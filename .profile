40ea9a3520d8fff7945f8e4a7c871abba1a926a9
ctronp <carlostp7@uc.cl>
2022-06-16 17:24:18 -0400

    godot install and add in $HOME/bin

--- .profile
+++ .profile
@@ -1,5 +1,6 @@
 
 . "$HOME/.cargo/env"
+export PATH=$PATH:/home/ctronp/bin
 
 # intel oneapi
 [ -f /opt/intel/oneapi/setvars.sh ] && source /opt/intel/oneapi/setvars.sh
--- godot.sh
+++ godot.sh
@@ -14,8 +14,10 @@ scons platform=linuxbsd target=release_debug module_mono_enabled=yes mono_glue=n
 bin/godot.linuxbsd.opt.tools.64.mono --generate-mono-glue modules/mono/glue
 scons platform=linuxbsd target=release_debug module_mono_enabled=yes bits=64
 cp -f misc/dist/linux/org.godotengine.Godot.desktop ~/.local/share/applications/
+cp -f $HOME/godot/bin/godot.linuxbsd.opt.tools.64.mono $HOME/bin/godot.mono
 
-sed -i "s|^Exec=godot|Exec=$HOME/godot/bin/godot.linuxbsd.opt.tools.64.mono|1" ~/.local/share/applications/org.godotengine.Godot.desktop
+
+sed -i "s|^Exec=godot|Exec=$HOME/godot.mono|1" ~/.local/share/applications/org.godotengine.Godot.desktop
 sed -i "s|^Icon=godot|Icon=$HOME/godot/icon.svg|1" ~/.local/share/applications/org.godotengine.Godot.desktop
 
 
