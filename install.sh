bd5d5fd453198cf7cf7313db54f60593aa68b1c0
ctronp <carlostp7@uc.cl>
2022-07-09 01:21:50 -0400

    default c and c++ compiler is gcc

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+set -xe
+
 # minimal install packages
-sudo dnf install git git-lfs clang clang lld
+sudo dnf install git clang lld
 
 # replace gcc with clang and ld with lld
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
-sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 50
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 100
+sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 50
 
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
-sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 50
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 100
+sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 50
+
+# Select default compiler
+sudo update-alternatives --config cc
+sudo update-alternatives --config c++
 
 sudo ln -sf /usr/bin/ld.lld /usr/bin/ld
