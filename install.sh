43eb330b95457da47c245feb9614ab4260e12ff1
ctronp <carlostp7@uc.cl>
2022-07-27 00:02:04 -0400

    select default linker

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 set -xe
 
 # minimal install packages
-sudo dnf install git clang lld xclip
+sudo dnf install git clang lld binutils-gold gcc xclip
 
 # replace gcc with clang and ld with lld
 sudo rm -rf /usr/bin/cc /usr/bin/c++ /usr/bin/ld
@@ -14,8 +14,10 @@ sudo update-alternatives --install /usr/bin/cc cc /usr/bin/clang 50
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/g++ 100
 sudo update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 50
 
+sudo update-alternative --install /usr/bin/ld ld /usr/bin/ld/ld.gold 100
+sudo update-alternative --install /usr/bin/ld ld /usr/bin/ld/ld.lld 50
+
 # Select default compiler
 sudo update-alternatives --config cc
 sudo update-alternatives --config c++
-
-sudo ln -sf /usr/bin/ld.lld /usr/bin/ld
+sudo update-alternatives --config ld
