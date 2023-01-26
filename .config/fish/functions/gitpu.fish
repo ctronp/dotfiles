f14d09109ad1bdc399865d512e8d572a965ecdfb
ctronp <carlostp7@uc.cl>
2023-01-26 16:11:16 -0300

    probably don't work

--- .config/fish/functions/gitpu.fish
+++ .config/fish/functions/gitpu.fish
@@ -1,3 +1,3 @@
-function gitpu --wraps='git pull --depth 1' --description 'alias gitpu git pull --depth 1'
-  git pull --depth 1 $argv; 
+function gitpu --wraps='git fetch --depth 1' --wraps='git pull --rebase --depth 1' --description 'alias gitpu git pull --rebase --depth 1'
+  git pull --rebase --depth 1 $argv; 
 end
