cf107b0c052e51d47ea12bb5716fbfa28839b28c
Carlos Troncoso <67880687+ctronp@users.noreply.github.com>
2023-02-03 16:57:40 -0300

    UE5 --depth 1

--- scripts/ue5.sh
+++ scripts/ue5.sh
@@ -8,7 +8,7 @@ printf "prefer downloading from https://www.unrealengine.com/en-US/linux if poss
 
 if [[ ! -f $HOME/ue5 ]]
 then 
-    git clone --single-branch -b release git@github.com:EpicGames/UnrealEngine.git ue5
+    git clone --depth 1 --single-branch -b release git@github.com:EpicGames/UnrealEngine.git ue5
 fi
 
 cd ~/ue5
