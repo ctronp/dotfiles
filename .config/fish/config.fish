2ea84f0a13113d43cf8c14d463e85a8026bc37e9
ctronp <carloscasa7@outlook.cl>
2023-04-27 20:47:18 -0400

    conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,11 @@ fenv source ~/.profile
 if status is-interactive
     starship init fish | source
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/ctronp/miniconda3/bin/conda
+    eval /home/ctronp/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
